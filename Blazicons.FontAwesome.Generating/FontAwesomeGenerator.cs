d3c4602c787a964d2c6275d41b75e15dce13e800
Kyle Herzog <holler@kyleherzog.com>
2022-12-30 12:04:59 -0600

    Initial commit

---
+++ Blazicons.FontAwesome.Generating/FontAwesomeGenerator.cs
@@ -0,0 +1,34 @@
+﻿using Blazicons.Generating;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.Threading;
+
+namespace Blazicons.FontAwesome.Generating;
+
+[Generator]
+internal class FontAwesomeGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        using var taskContext = new JoinableTaskContext();
+        var taskFactory = new JoinableTaskFactory(taskContext);
+        var downloader = new RepoDownloader(new Uri(Properties.Resources.DownloadAddress));
+        taskFactory.Run(
+            async () =>
+            {
+                await downloader.Download(@"^svgs\/.*.svg$").ConfigureAwait(true);
+            });
+
+        var svgFolder = Path.Combine(downloader.ExtractedFolder, $"{downloader.RepoName}-{downloader.BranchName}", "svgs", "regular");
+        context.WriteIconsClass("FontAwesomeRegularIcon", svgFolder);
+
+        svgFolder = Path.Combine(downloader.ExtractedFolder, $"{downloader.RepoName}-{downloader.BranchName}", "svgs", "solid");
+        context.WriteIconsClass("FontAwesomeSolidIcon", svgFolder);
+
+        downloader.CleanUp();
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        // required by ISourceGenerator
+    }
+}
